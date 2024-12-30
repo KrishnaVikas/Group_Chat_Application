969eb5ac2f31442db1d46d7cb4c4e04bf32eb83b
Krishna Vikas <krishnavikas2000@gmail.com>
2024-12-30 11:41:05 +0530

    Modified config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,18 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const config = {
-    app: {
-      port: 3000
-    },
-    db: {
-      name: 'groupChatApp',
-      baseUrl: "0.0.0.0",
-      port: "27017"
-    },
-    saltRounds: 10,
-    JWT_SECRET: "e0e584dfe73dda6850719054ed51e2e92c07eaa5f494928b0fbcbf0d35527b02"
+  app: {
+    port: process.env.APP_PORT || 3000, // Default to 3000 if not provided
+  },
+  db: {
+    name: process.env.DB_NAME,
+    baseUrl: process.env.DB_BASE_URL,
+    port: process.env.DB_PORT,
+  },
+  saltRounds: parseInt(process.env.SALT_ROUNDS, 10), // Convert to a number
+  JWT_SECRET: process.env.JWT_SECRET,
 };
 
 export default config;
